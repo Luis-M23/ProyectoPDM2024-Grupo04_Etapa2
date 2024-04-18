25ea10df9583923e390f9705768e015c5f4e6ec7
Kevin Grande <kevinbussines2020@gmail.com>
2024-04-18 08:17:16 -0600

    ADD: Navigation bar

--- app/src/main/java/com/pops/z_gaming/MainActivity.kt
+++ app/src/main/java/com/pops/z_gaming/MainActivity.kt
@@ -3,13 +3,47 @@ package com.pops.z_gaming
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
+import com.pops.z_gaming.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
 
-        // Agrega código de depuración
-        Log.d("MainActivity", "onCreate() called")
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.fav -> replaceFragment(Favorites())
+                R.id.cart -> replaceFragment(ShoppingCart())
+                R.id.profile -> replaceFragment(Profile())
+
+                else ->{
+
+
+                }
+
+            }
+
+            true
+
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
     }
 }
