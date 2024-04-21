3cf2274a14961613ee0ed16cd857da5dc01f6104
PatriciaFlores1 <fb21010@ues.edu.sv>
2024-04-21 17:57:44 -0600

    Add fragments profile, restore password and details product.

---
+++ app/src/main/java/com/pops/z_gaming/DetailsFragment.kt
@@ -0,0 +1,14 @@
+package com.pops.z_gaming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+
+class DetailsFragment : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.fragment_details)
+//    }
+}
+
