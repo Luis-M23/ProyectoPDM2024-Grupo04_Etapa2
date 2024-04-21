a52ff825806c76f40e004a0ba6c2f0a5118fa016
PatriciaFlores1 <fb21010@ues.edu.sv>
2024-04-21 15:53:27 -0600

    Add detalle de compras

---
+++ app/src/main/java/com/pops/z_gaming/DetailsBuyFragment.kt
@@ -0,0 +1,12 @@
+package com.pops.z_gaming
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+
+class DetailsBuyFragment : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.fragment_details_buy)
+//    }
+}
