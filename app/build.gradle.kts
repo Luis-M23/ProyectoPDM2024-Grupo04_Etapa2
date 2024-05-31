85ce9a9a70425a279f87a222387dc63d40c4e0a8
PatriciaFlores1 <fb21010@ues.edu.sv>
2024-05-31 13:01:52 -0600

    gitflow-feature-stash: WebService
    Add webservice and add API connection with retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,10 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("com.github.bumptech.glide:glide:4.14.2")
 
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
     implementation("androidx.activity:activity:1.6.1")
 }
---
+++ app/src/main/java/com/pops/z_gaming/WebService.kt
@@ -0,0 +1,70 @@
+package com.pops.z_gaming
+
+import com.google.gson.GsonBuilder
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+//conexion  a la API de forma local
+object AppConstantes {
+    const val BASE_URL = "http://10.0.2.2:3000"
+}
+
+public interface WebService {
+
+    //Obtener lista de productos
+    @GET("/productos")
+    suspend fun obtenerProductos(): Response<List<Producto>>
+
+    //Obtener producto por ID
+    @GET("/producto/{idProducto}")
+    suspend fun obtenerProducto(
+        @Path("idProducto") idProducto: Int
+    ): Response<Producto>
+
+    //Obtener productos por categoria
+    @GET("/productos/categoria/{idCategoria}")
+    suspend fun obtenerProductosPorCategoria(
+        @Path("idCategoria") idCategoria: Int
+    ): Response<List<Producto>>
+
+    //Agregar producto
+    @POST("/producto/add")
+    suspend fun agregarProducto(
+        @Body producto: Producto
+    ): Response<String>
+
+    //Actualizar producto
+    @PUT("/producto/update/{idProducto}")
+    suspend fun actualizarProducto(
+        @Path("idProducto") idProducto: Int,
+        @Body producto: Producto
+    ): Response<String>
+
+    //Obtener productos favoritos
+    @GET("/productos/favoritos")
+    suspend fun obtenerProductosFavoritos(): Response<List<Producto>>
+
+    //Eliminar producto por ID
+    @DELETE("/producto/delete/{idProducto}")
+    suspend fun borrarProducto(
+        @Path("idProducto") idProducto: Int
+    ): Response<String>
+}
+
+//configuración de retrofit
+object RetrofitClient {
+    val webService: WebService by lazy {
+        Retrofit.Builder()
+            .baseUrl(AppConstantes.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+            .create(WebService::class.java)
+    }
+}
